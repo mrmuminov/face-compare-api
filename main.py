502a05e1cedbf66fd95ad976c4a5d2f8e82a1eef
bahriddin <darkshadeuz@gmail.com>
2023-08-27 16:20:11 +0500

    Add docs

--- main.py
+++ main.py
@@ -1,33 +1,62 @@
-from flask import Flask, request
-from random import randint
+import base64
+import io
+import re
+import time
+
 import face_recognition
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
-@app.route(rule="/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET'])
 def main():
-    return {
+    return jsonify({
         "success": True,
         "message": "Server is working",
-    }
+    })
 
 
-@app.route(rule="/compare", methods=['POST'])
+@app.route("/compare", methods=['POST'])
 def compare():
-    r = randint(a=0, b=10)
-    passport_img_path = 'uploads/' + str(r) + '_passport.jpg'
-    face_img_path = 'uploads/' + str(r) + '_face.jpg'
-    passport = request.files['passport']
-    face = request.files['face']
-    passport.save(passport_img_path)
-    face.save(face_img_path)
-    known_image = face_recognition.load_image_file(passport_img_path)
-    unknown_image = face_recognition.load_image_file(face_img_path)
-    biden_encoding = face_recognition.face_encodings(known_image)[0]
-    unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
-    results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
-    return {
-        "success": results,
-        "message": "It's same" if results else "Images saved",
-    }
+    try:
+        data = request.json  # Assuming JSON request
+        passport_base64 = data.get('passport')
+        face_base64 = data.get('face')
+
+        def decode_base64(data):
+            image_data = re.sub('^data:image/.+;base64,', '', data)
+            return base64.b64decode(image_data)
+
+        start_time = time.time()
+
+        passport_bytes = decode_base64(passport_base64)
+        face_bytes = decode_base64(face_base64)
+
+        known_image = face_recognition.load_image_file(io.BytesIO(passport_bytes))
+        unknown_image = face_recognition.load_image_file(io.BytesIO(face_bytes))
+
+        biden_encoding = face_recognition.face_encodings(known_image)[0]
+        unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
+
+        comparison_start_time = time.time()
+        results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
+        comparison_time = time.time() - comparison_start_time
+
+        return jsonify({
+            "success": True,
+            "match": bool(results[0]),
+            "message": "It's the same person" if results[0] else "It's not the same person",
+            "image_save_time": comparison_start_time - start_time,
+            "comparison_time": comparison_time,
+        })
+
+    except Exception as e:
+        return jsonify({
+            "success": False,
+            "message": str(e),
+        })
+
+
+if __name__ == "__main__":
+    app.run()
